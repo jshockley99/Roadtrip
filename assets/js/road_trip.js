c36fa074999ff8700e0ac44346ac6805bdb8e125
Ovie <you@example.com>
2017-12-06 10:07:53 -0500

    adds spotify button

--- assets/js/road_trip.js
+++ assets/js/road_trip.js
@@ -1,35 +1,63 @@
+$(document).ready(function() {
 
+    /*
+    	var apiKey = "AIzaSyBaNQW3FobOncUto_UN8kX1wDhI8JzJKcA"
+    	var queryURL = "https://maps.googleapis.com/maps/api/directions/json"
+    	
+    	// Use $.param to nicely format the url sting
+    	queryURL += "?" + $.param({           
+    		'origin': "Atlanta",
+    		'destination': "New York City", 
+    		'key': apiKey
+    		
+    	});
 
-$(document).ready(function () {
+    console.log(queryURL);
 
+    	$.ajax({
+    	  url: 'https://maps.googleapis.com/maps/api/directions/json?origin=Disneyland&destination=Universal+Studios+Hollywood4&key=AIzaSyCF5c2ISZ8-lpbZ3eTYEEHlfjd3-AxmXS0&Origin="anonymous"',
+    	}).done(function(result) {	
+    	  console.log(result);    
+    	  	
+    	}).fail(function(err) {
+    	  throw err;
+    	});
 
-	var apiKey = "AIzaSyBaNQW3FobOncUto_UN8kX1wDhI8JzJKcA"
-	var queryURL = "https://maps.googleapis.com/maps/api/directions/json"
-	
-	// Use $.param to nicely format the url sting
-	queryURL += "?" + $.param({           
-		'origin': "Atlanta",
-		'destination': "New York City", 
-		'key': apiKey
-		
-	});
+    */
 
-console.log(queryURL);
+    //spotify api calls
+    //for each city, use spotify url to get playlists
 
-	$.ajax({
-	  url: 'https://maps.googleapis.com/maps/api/directions/json?origin=Disneyland&destination=Universal+Studios+Hollywood4&key=AIzaSyCF5c2ISZ8-lpbZ3eTYEEHlfjd3-AxmXS0&Origin="anonymous"',
-	}).done(function(result) {	
-	  console.log(result);    
-	  	
-	}).fail(function(err) {
-	  throw err;
-	});
+    //get spotify authorisation
+    var clientID = "4a7d4aa309ce40a9b644635d2e74b1bb";
+    var clientSecret = "e85c7c6bd60c48d1986be1d5b6b3095c";
+    var scope = "playlist-modify-public";
+    redirectUri = 'https://jshockley99.github.io/Roadtrip/index.html';
+    var spotifyAuthUrl = 'https://accounts.spotify.com/authorize?client_id=' + clientID + '&redirect_uri=' + redirectUri + '&scope=' + scope + '&response_type=token';
 
+    //spotify auth ajax pull on clicking authorise button
+    // $("#spotAuth").on("click", function(event) {
+    //     event.preventDefault();
 
+    //     $.ajax({
+    //         method: "GET",
+    //         url: 'https://cors-anywhere.herokuapp.com/' + spotifyAuthUrl
+    //     }).done(function(response) {
+    //         console.log(response);
+    //     });
+    // });
+
+    /*var accessToken = 'BQDefyVOD_4MEBrMlMoVYU3Zl8klNnlJ62_cCTNfrwqekhDr1v9C7MJqikb4jShwGDz2g117ZgyHNUCkchGuBvtQLXO1CxX2LMKhF2mwcgS7RKbu1_0s_16xUiaRfvEKGMaQE-TF'
+    $.ajax({
+       url: 'https://api.spotify.com/v1/me',
+       headers: {
+           'Authorization': 'Bearer ' + accessToken
+       },
+       success: function(response) {
+           console.log(response);
+       }
+    });
+    */
 
 
 });
-
-
-
-	
