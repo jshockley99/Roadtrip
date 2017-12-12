2b6130864cf70a302ae37a91f43ebd74e22c3e13
Ovie <you@example.com>
2017-12-12 00:59:30 -0500

    tweaks playlist array to include owner id

--- assets/js/road_trip.js
+++ assets/js/road_trip.js
@@ -18,6 +18,7 @@ $(document).ready(function() {
     var city;
     var currentPlaylistId;
     var playlistArray = [];
+    var playlistOwner;
 
 
     function displayCities() {
@@ -113,7 +114,9 @@ $(document).ready(function() {
                     //loop through data array and push new playlists into playlist array
                     for (var i = 0; i < data.length; i++) {
                         var playlistID = data[i].id;
-                        playlistArray.push(playlistID);
+                        var playlistCreator = data[i].owner.id;
+                        var playlistAndCreatorArray= [playlistID,playlistCreator];
+                        playlistArray.push(playlistAndCreatorArray);
                     }
                     console.log(playlistArray);
                     //create object using city and playlistArray as key value pairs
@@ -137,7 +140,7 @@ $(document).ready(function() {
             console.log(currentPlaylistId);
             console.log(userId);
             var iframeReqs = ' width="300" height="380" frameborder="0" allowtransparency="true"';
-            var iframeLink = "https://open.spotify.com/embed?uri=spotify:user:"+userId+":spotify:playlist:"+currentPlaylistId+"&theme=white&view=coverart"+iframeReqs;
+            var iframeLink = "https://open.spotify.com/embed?uri=spotify:user:"+playlistOwner+":spotify:playlist:"+currentPlaylistId+"&theme=white&view=coverart"+iframeReqs;
             iframeFinal = '<iframe src=' + iframeLink + '></iframe>';
             console.log(iframeFinal);
             $("#playlist-page").html(iframeFinal);
